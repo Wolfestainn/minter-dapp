8e01b93fc3c03259e5a4723e60eba27c9139d723
Wolfestainn <wolfestainngaming@gmail.com>
2022-05-12 21:25:02 +0300

    rinkeby test

--- backend/src/config.js
+++ backend/src/config.js
@@ -56,7 +56,7 @@ const OWNER_ADDRESS = '0x03A9Fbf58281014FB26fb174a0a80690525001fb';
 const TREASURY_ADDRESS = '0x03A9Fbf58281014FB26fb174a0a80690525001fb';
 const MAX_SUPPLY = 5000; // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
 const MINT_PRICE = 7; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. CANNOT BE UPDATED!
-const TOKENS_PER_MINT = 5; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
+const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
 const PUBLIC_MINT_START_DATE = "2022-03-20T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
